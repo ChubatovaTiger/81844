eff2103b8c88fc13ffcea4a579a7b6f3566ffb9c
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2023-06-22 22:09:58 +0200

    Update UnitTest1.cs

--- TW-81844-reproduce.Test/UnitTest1.cs
+++ TW-81844-reproduce.Test/UnitTest1.cs
@@ -2,15 +2,15 @@ namespace TW_81844_reproduce.Test {
     [TestClass]
     public class UnitTest1 {
         [TestMethod]
-        [DataRow(0, 1, 5000)]
-        [DataRow(0, 2, 4000)]
-        [DataRow(0, 3, 6000)]
-        [DataRow(0, 34, 4000)]
-        [DataRow(0, 35, 10000)]
-        [DataRow(0, 36, 5000)]
-        [DataRow(0, 37, 5000)]
-        [DataRow(0, 38, 10000)]
-        [DataRow(0, 39, 10000)]
+        [DataRow(0, 1, 500)]
+        [DataRow(0, 2, 400)]
+        [DataRow(0, 3, 600)]
+        [DataRow(0, 34, 400)]
+        [DataRow(0, 35, 1000)]
+        [DataRow(0, 36, 500)]
+        [DataRow(0, 37, 500)]
+        [DataRow(0, 38, 1000)]
+        [DataRow(0, 39, 1000)]
         public async Task TestMethod11(int first, int second, int delay) {
             var sum = MathLocal.Sum(first, second);
 
@@ -19,10 +19,10 @@ namespace TW_81844_reproduce.Test {
         }
 
         [TestMethod]
-        [DataRow(0, 4, 5500)]
-        [DataRow(0, 5, 5000)]
-        [DataRow(0, 6, 7000)]
-        [DataRow(0, 7, 5000)]
+        [DataRow(0, 4, 550)]
+        [DataRow(0, 5, 500)]
+        [DataRow(0, 6, 700)]
+        [DataRow(0, 7, 500)]
         public async Task TestMethod12(int first, int second, int delay) {
             var sum = MathLocal.Sum(first, second);
 
@@ -31,10 +31,10 @@ namespace TW_81844_reproduce.Test {
         }
 
         [TestMethod]
-        [DataRow(0, 22, 6000)]
-        [DataRow(0, 23, 5500)]
-        [DataRow(0, 24, 5000)]
-        [DataRow(0, 25, 7000)]
+        [DataRow(0, 22, 600)]
+        [DataRow(0, 23, 550)]
+        [DataRow(0, 24, 500)]
+        [DataRow(0, 25, 700)]
         public async Task TestMethod13(int first, int second, int delay) {
             var sum = MathLocal.Sum(first, second);
 
@@ -67,14 +67,14 @@ namespace TW_81844_reproduce.Test {
         }
 
         [TestMethod]
-        [DataRow(0, 32, 3000)]
-        [DataRow(0, 33, 4000)]
-        [DataRow(0, 34, 4000)]
-        [DataRow(0, 35, 10000)]
-        [DataRow(0, 36, 5000)]
-        [DataRow(0, 37, 5000)]
-        [DataRow(0, 38, 10000)]
-        [DataRow(0, 39, 10000)]
+        [DataRow(0, 32, 300)]
+        [DataRow(0, 33, 400)]
+        [DataRow(0, 34, 400)]
+        [DataRow(0, 35, 1000)]
+        [DataRow(0, 36, 500)]
+        [DataRow(0, 37, 500)]
+        [DataRow(0, 38, 1000)]
+        [DataRow(0, 39, 1000)]
         public async Task TestMethod16(int first, int second, int delay) {
             var sum = MathLocal.Sum(first, second);
 
@@ -83,11 +83,11 @@ namespace TW_81844_reproduce.Test {
         }
 
         [TestMethod]
-        [DataRow(0, 29, 3500)]
-        [DataRow(0, 30, 1000)]
-        [DataRow(0, 31, 5000)]
-        [DataRow(0, 32, 3000)]
-        [DataRow(0, 33, 4000)]
+        [DataRow(0, 29, 350)]
+        [DataRow(0, 30, 100)]
+        [DataRow(0, 31, 500)]
+        [DataRow(0, 32, 300)]
+        [DataRow(0, 33, 400)]
         public async Task TestMethod17(int first, int second, int delay) {
             var sum = MathLocal.Sum(first, second);
 

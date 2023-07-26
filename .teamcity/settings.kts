567ec1a6fde9f8c57de4c66b3aa7fc086771c063
admin <admin@TeamCity>
2023-07-26 11:35:48 +0200

    TeamCity change in '81844' project: 'Test' build configuration settings were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -108,7 +108,6 @@ object Test : BuildType({
             sdk = "6"
         }
         dotnetTest {
-            enabled = false
             projects = "TW-81844-reproduce.Test/TW-81844-reproduce.Test.csproj"
             sdk = "6"
         }
